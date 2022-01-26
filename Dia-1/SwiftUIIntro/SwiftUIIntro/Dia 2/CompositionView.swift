be3f16dfa8c8a97a0bf69a6ef01f55754289c74b
JOSE LUIS BUSTOS ESTEBAN <joselbe@MacBook-Pro-de-JOSE.local>
2022-01-26 19:09:34 +0100

    Dia 1

---
+++ Dia-1/SwiftUIIntro/SwiftUIIntro/Dia 2/CompositionView.swift
@@ -0,0 +1,20 @@
+//
+//  CompositionView.swift
+//  SwiftUIIntro
+//
+//  Created by JOSE LUIS BUSTOS ESTEBAN on 25/1/22.
+//
+
+import SwiftUI
+
+struct CompositionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompositionView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompositionView()
+    }
+}
