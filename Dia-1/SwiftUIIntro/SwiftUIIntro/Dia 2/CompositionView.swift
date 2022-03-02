f4bd5dcbd9449392033acb6119c214f093c01996
Jose Luis Bustos <bejl@outlook.es>
2022-03-02 12:30:17 +0100

    example added

--- Dia-1/SwiftUIIntro/SwiftUIIntro/Dia 2/CompositionView.swift
+++ Dia-1/SwiftUIIntro/SwiftUIIntro/Dia 2/CompositionView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct CompositionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack{
+                Text("Hola")
+            }
+            .frame(width: 200, height: 200)
+            .background(Color.blue)
+            .padding([.top],100)
+            
+            Text("Hola")
+                .offset(x: 30)
+            
+            Spacer()
+            
+            Button {
+                //Action here
+            } label: {
+                HStack{
+                    Spacer()
+                    Text("Hacer login")
+                        .padding()
+                    Spacer()
+                }
+            }
+            .foregroundColor(Color.white)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .padding([.bottom],40)
+            .padding([.leading, .trailing],20)
+            
+    
+            
+            
+        }
+
+        
     }
 }
 
