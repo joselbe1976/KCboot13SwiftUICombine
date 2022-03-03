472578a4de29d06d5e2d99a3597fdc20d7c6df87
Jose Luis Bustos <bejl@outlook.es>
2022-03-03 19:42:04 +0100

    Day 3

---
+++ Dia-3/kcHerosBoot13/Shared/kcHerosBoot13App.swift
@@ -0,0 +1,17 @@
+//
+//  kcHerosBoot13App.swift
+//  Shared
+//
+//  Created by JOSE LUIS BUSTOS ESTEBAN on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct kcHerosBoot13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
