5bf78ec94807f9564130761b3d1d1f67d72e2f61
Jose Luis Bustos <bejl@outlook.es>
2022-03-03 20:05:57 +0100

    Proyecto listo para empezar. Ya esta configurado

---
+++ Dia-3/kcHerosBoot13/Shared/Tools/KeyChain.swift
@@ -0,0 +1,37 @@
+//
+//  KeyChain.swift
+//  kcHerosBoot13
+//
+//  Created by JOSE LUIS BUSTOS ESTEBAN on 3/3/22.
+//
+
+import Foundation
+import KeychainSwift
+
+//Guardado en hey chain
+func saveKC(key:String, value:String) -> Bool {
+    if let data = value.data(using: .utf8){
+        let keychain = KeychainSwift()
+        keychain.set(data, forKey: key)
+        return true
+    } else{
+        return false
+    }
+}
+
+//Cargar del KeyChain
+func loadKC(key:String) -> String? {
+    let keychain = KeychainSwift()
+    if let data = keychain.get(key){
+        return data
+    } else{
+        return ""
+    }
+}
+
+
+// Eliminar
+func deleteKC(key:String){
+    let keychain = KeychainSwift()
+    keychain.delete(key)
+}
---
+++ Dia-3/kcHerosBoot13/Shared/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  kcHerosBoot13
+//
+//  Created by JOSE LUIS BUSTOS ESTEBAN on 3/3/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+            .environment(\.locale, .init(identifier: "en"))
+    }
+}
